cac1b115ad0df7b5fa38fe085c4f9592295a2ac1
pajtoo <147297402+pajtoo@users.noreply.github.com>
2024-10-25 17:36:15 +0200

    Added SLF4J dependencies

---
+++ src/main/java/cz/itnetwork/evidencepojisteni/exception/MyUncaughtExceptionHandler.java
@@ -0,0 +1,15 @@
+package cz.itnetwork.evidencepojisteni.exception;
+
+import cz.itnetwork.evidencepojisteni.controller.InsuredController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(InsuredController.class);
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        log.error("An uncaught exception has occured: ", e);
+        System.out.println("V aplikaci se vyskytla chyba, obraťte se prosím na zákaznickou podporu.");
+    }
+}
